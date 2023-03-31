0e9e5b6792bee20da39ec9fb07350d6a115fbdc3
davecamp7744 <92772510+davecamp7744@users.noreply.github.com>
2023-03-30 19:46:38 -0600

    colores chat

--- chatbotones/static/js/main.js
+++ chatbotones/static/js/main.js
@@ -10,8 +10,5 @@ $.ajax({
 }
 const button = document.querySelector('#mi-boton');
 button.addEventListener('click', () => {
-    // Agrega aquí el código que se ejecuta después de hacer clic en el botón
-
-    // Deshabilita el botón después de hacer clic
     button.style.display = 'none';
-    });
+});
